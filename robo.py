b2f9f6e5c35a98d65b8c453f77a97e3bb290f43a
JuaelsonRamos <juaelsonjk@gmail.com>
2023-06-24 10:41:23 -0300

    xpaths organizados para melhor legibilidade

--- robo.py
+++ robo.py
@@ -7,22 +7,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from threading import Thread
+from dataclasses import dataclass
 # from selenium.webdriver.support import expected_conditions as EC
 import pyautogui
 import time
 import pandas as pd
 
-def click_caixa_de_dialogo(driver):
+LINK_PRINCIPAL = 'https://sso.acesso.gov.br/login?client_id=login.esocial.gov.br&authorization_id=188b4b3efd4'
 
-    #wait.until(expected_conditions.alert_is_present())
-    caixa_dialogo = Alert(driver)
-    
-
-    # caixa_dialogo = driver.switch_to.alert
-    caixa_dialogo.accept()
-
-
+@dataclass(init=False, frozen=True)
+class Caminhos:
+    ESOCIAL_BOTAO_LOGIN: str = '//*[@id="login-acoes"]/div[2]/p/button'
+    SELECIONAR_CERTIFICADO: str = '//*[@id="cert-digital"]/button'
+    ESOCIAL_TROCAR_PERFIL: str = '//*[@id="header"]/div[2]/a'
+    ESOCIAL_ACESSAR_PERFIL: str = '//*[@id="perfilAcesso"]'
+    ESOCIAL_CNPJ_INPUT: str = '//*[@id="procuradorCnpj"]'
+    ESOCIAL_CNPJ_INPUT_CONFIRMAR: str = '//*[@id="btn-verificar-procuracao-cnpj"]'
+    ESOCIAL_CNPJ_SELECIONAR_MODULO: str = '/html/body/div[3]/div[4]/div/form/div/section/div[7]/div[2]/div[6]'
+    ESOCIAL_MENU_TRABALHADOR: str = '//html/body/div[1]/div[2]/div[1]/nav/button'
+    ESOCIAL_MENU_OPCAO_EMPREGADOS: str = '/html/body/div[1]/div[2]/div[1]/nav/div/ul/li[1]/a'
+    ESOCIAL_CPF_EMPREGADO_INPUT: str = '/html/body/div[1]/div[2]/div[1]/div/div/div/div/div/div/div/div/input'
 
 driver = webdriver.Chrome('C:\chromedriver_win32 (2)')
 wait = WebDriverWait(driver, 20)
@@ -38,81 +42,61 @@ tabela = pd.read_excel("alfa.xlsx", header=None)
 coluna1 = tabela.iloc[2:, 54].values
 coluna2 = tabela.iloc[2:, 19].values
 # Exibir os valores da coluna
-print(coluna1)
-print(coluna2)
+# print(coluna1)
+# print(coluna2)
 
 # Iterar sobre os valores da coluna
-for CNPJ in coluna1:
+# for CNPJ in coluna1:
     # Faça algo com cada valor de CNPJ
-    print(CNPJ)
+    # print(CNPJ)
 
 if len(coluna1) != len(coluna2):
     raise ValueError(f"Não há a 1 cnpj para cada cpf, {len(coluna1)} cnpjs para {len(coluna2)} cpfs.")
+
     # Montando toda a estrutura do código
 for CNPJ, CPF in zip(coluna1, coluna2):
     # valor_coluna1 = CNPJ
     # Faça algo com cada valor de CNPJ da coluna específica
 
-    driver.get('https://sso.acesso.gov.br/login?client_id=login.esocial.gov.br&authorization_id=188b4b3efd4')
+    driver.get(LINK_PRINCIPAL)
 
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="login-acoes"]/div[2]/p/button')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_BOTAO_LOGIN)
     ativar_button.click()
     time.sleep(2)
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="cert-digital"]/button')
-    acao_paralela = Thread(name="clicar_caixa_de_dialogo",
-                           target=click_caixa_de_dialogo,
-    
-                           args=[driver])
-    acao_paralela.start()
+    ativar_button = driver.find_element(By.XPATH, Caminhos.SELECIONAR_CERTIFICADO)
     ativar_button.click()
-    acao_paralela.join() # esperar a ação terminar antes de prosseguir
-
-    # selecionar certificado
-    # wait.until(expected_conditions.alert_is_present())
-    # caixa_dialogo = Alert(driver)
-    # caixa_dialogo = driver.switch_to.alert
-    # caixa_dialogo.accept()
-    
-
 
     # Aguarde algum tempo após o clique (opcional)
     time.sleep(3)
 
-
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="header"]/div[2]/a')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_TROCAR_PERFIL)
     ativar_button.click()
     time.sleep(2)
 
-
-
-
     # ESCOLHA DE CNPJ, LER PLANILHA PRIMEIRO
-        
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="perfilAcesso"]')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_ACESSAR_PERFIL)
     ativar_button.click()
     time.sleep(2)
 
     pyautogui.press('down', presses=2)
     pyautogui.press('enter')
 
-
-
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="procuradorCnpj"]').send_keys(CNPJ)
-    ativar_button = driver.find_element(By.XPATH, '//*[@id="btn-verificar-procuracao-cnpj"]')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_CNPJ_INPUT).send_keys(CNPJ)
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_CNPJ_INPUT_CONFIRMAR)
     ativar_button.click()
     time.sleep(2)
-    ativar_button = driver.find_element(By.XPATH, '/html/body/div[3]/div[4]/div/form/div/section/div[7]/div[2]/div[6]')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_CNPJ_SELECIONAR_MODULO)
     ativar_button.click()
 
     time.sleep(5)
 
-    ativar_button = driver.find_element(By.XPATH, '//html/body/div[1]/div[2]/div[1]/nav/button')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_MENU_TRABALHADOR)
     ativar_button.click()
-    ativar_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/nav/div/ul/li[1]/a')
+    ativar_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_MENU_OPCAO_EMPREGADOS)
     ativar_button.click()
     time.sleep(5)
 
-    input_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div/div/div/div/div/div/div/input')
+    input_button = driver.find_element(By.XPATH, Caminhos.ESOCIAL_CPF_EMPREGADO_INPUT)
     input_button.send_keys(CPF)
     time.sleep(2)
     pyautogui.press('enter')
