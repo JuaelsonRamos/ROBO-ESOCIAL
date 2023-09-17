9a3e6cc358f36d2720bb885fb6719707b14cd932
Marcos Delay <dev.marcosdly@gmail.com>
2023-09-17 14:18:45 -0300

    Namespace compartilhado entre processos para salvar estado do processamento

--- src/async_vitals/messaging.py
+++ src/async_vitals/messaging.py
@@ -1,10 +1,18 @@
 """Objetos que prossibilitam ou facilitam a comunicações entre unidades de concorrência (threads,
 processos, coroutines)."""
 
-from dataclasses import dataclass
+from ctypes import Structure, c_int32, c_long, c_longlong, string_at, sizeof, memmove
 from aioprocessing import AioQueue
+from dataclasses import dataclass
 
-__all__ = ["Queues"]
+__all__ = [
+    "ProgressStateNamespace",
+    "Queues",
+    "STR_BASE_TYPE",
+    "STR_BUFSIZE",
+    "STR_DUMMY",
+    "STR_TYPE",
+]
 
 
 @dataclass(init=False, frozen=True)
@@ -22,3 +30,44 @@ class Queues:
 
     planilhas_para_depois = AioQueue()
     """Fila de planilhas que deram erro no hora de salvar e o usuário optou por salvá-las depois."""
+
+
+STR_BUFSIZE: int = 1024
+STR_BASE_TYPE = c_int32
+STR_TYPE = STR_BASE_TYPE * STR_BUFSIZE
+STR_DUMMY = STR_TYPE(*([ord("\0")] * 10))
+
+
+class ProgressStateNamespace(Structure):
+    # See: https://stackoverflow.com/a/5352531/15493645
+
+    _fields_ = [
+        ("cnpj_last_updated_ns", c_longlong),
+        ("cnpj_max_last_updated_ns", c_longlong),
+        ("cnpj_current", c_long),
+        ("cnpj_max", c_long),
+        ("cpf_last_updated_ns", c_longlong),
+        ("cpf_max_last_updated_ns", c_longlong),
+        ("cpf_current", c_long),
+        ("cpf_max", c_long),
+        ("cnpj_msg", STR_TYPE),
+        ("cnpj_long_msg", STR_TYPE),
+        ("cpf_msg", STR_TYPE),
+        ("cpf_long_msg", STR_TYPE),
+        ("general_msg", STR_TYPE),
+    ]
+
+    @classmethod
+    def get_string(cls, value: STR_TYPE) -> str:
+        return string_at(value, sizeof(value)).decode("utf-32").replace("\0", "")
+
+    @classmethod
+    def set_string(cls, original_value: STR_TYPE, value: str) -> None:
+        size = len(value)
+        if size > STR_BUFSIZE:
+            raise ValueError("String é muito grande para o buffer.")
+        memmove(
+            original_value,
+            (STR_BASE_TYPE * size)(*[ord(c) for c in value]),
+            sizeof(STR_BASE_TYPE) * size,
+        )
