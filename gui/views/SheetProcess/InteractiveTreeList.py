ba556218d8bb1285d9e0efeac746ac145818a043
Marcos Bião <dev.marcosdly@gmail.com>
2024-10-10 04:04:49 -0300

    refactor(gui): ProcessQueue: convertido em genérica e modular

---
+++ gui/views/SheetProcess/InteractiveTreeList.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from gui.views.SheetProcess.QueueButtonRow import QueueButtonRow
+from gui.views.SheetProcess.ShortTree import ShortTree
+from gui.views.SheetProcess.data import Heading
+
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class InteractiveTreeList(ttk.Frame):
+    def __init__(self, master, title: str, *, columns: list[Heading]):
+        super().__init__(master)
+        self.title = title
+        self.pack(fill=tk.X, side=tk.TOP, anchor=tk.NW)
+        self.button_row = QueueButtonRow(self, self.title)
+        self.tree = ShortTree(self, columns=columns)
+
+    def add_button(self, text: str):
+        self.button_row.add_button(text)
--- gui/views/SheetProcess/ProcessingQueue.py
+++ gui/views/SheetProcess/ProcessingQueue.py
@@ -2,25 +2,19 @@ from __future__ import annotations
 
 import gui.views.SheetProcess.data as data
 
-from gui.views.SheetProcess.QueueButtonRow import QueueButtonRow
-from gui.views.SheetProcess.ShortTree import ShortTree
+from gui.views.SheetProcess.InteractiveTreeList import InteractiveTreeList
 
 import tkinter as tk
 import tkinter.ttk as ttk
 
 
-class ProcessingQueue(ttk.Frame):
+class ProcessingQueue(InteractiveTreeList):
     def __init__(self, master):
-        super().__init__(master)
-        self.pack(fill=tk.X, side=tk.TOP, anchor=tk.NW)
-        self.create_widgets()
-
-    def create_widgets(self):
-        button_row = QueueButtonRow(self, 'Fila de Processamento')
-        button_row.add_button('Adicionar')
-        button_row.add_button('Remover')
-        button_row.add_button('Começar')
-        button_row.add_button('Pausar')
-        button_row.add_button('Parar')
-
-        ShortTree(self, columns=data.input_queue.columns)
+        super().__init__(
+            master, 'Fila de Processamento', columns=data.input_queue.columns
+        )
+        self.add_button('Adicionar')
+        self.add_button('Remover')
+        self.add_button('Começar')
+        self.add_button('Pausar')
+        self.add_button('Parar')
