5e7e8cb1cb2e0d179f12333419840533cd1e9e45
Marcos Bião <dev.marcosdly@gmail.com>
2024-10-30 12:14:07 -0300

    feat(util): construtor de classes singleton

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,8 +1,22 @@
 from __future__ import annotations
 
+import abc
+import sys
+import inspect
+
 
 def block(function):
     """Decorador de transforma uma definição de função em um
     bloco de código anônimo. A função não será definida e
     será imediatamente executada."""
     function()
+
+
+def singleton(cls):
+    assert not inspect.isbuiltin(cls)
+    assert inspect.getmodule(cls) is not sys.modules['builtins']
+    assert inspect.isclass(cls)
+    assert abc.ABCMeta not in cls.__bases__
+    assert abc.ABC not in cls.__bases__
+    assert callable(cls)
+    return cls()
