45b838ef36add744d45be524251ec4c96b695bcf
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-02 10:43:08 -0300

    feat(models): add model Row para valores de células

---
+++ sistema/models/Row.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from sistema.models import ColumnMetadata
+from sistema.spreadsheet import QualifiedType, QualifiedValue, Requirement
+from sistema.validators import Validator
+
+from pydantic import BaseModel
+
+
+class Row(BaseModel, frozen=True, strict=True):
+    index: int
+    property_name: str
+    required: Requirement
+    is_opcional: bool
+    is_empty: bool
+    qualified_type: QualifiedType
+    validator_clas: Validator
+    original_value: str
+    qualified_value: QualifiedValue
+    columns_metadata: ColumnMetadata
