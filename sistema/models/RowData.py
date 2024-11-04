884d01255ca19534f80a5da26e97d6bdbd103924
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-04 17:04:55 -0300

    fix(RowData): ignorando propriedade genérica

--- sistema/models/RowData.py
+++ sistema/models/RowData.py
@@ -8,7 +8,9 @@ import sistema.spreadsheet as sheet
 from sistema.validators import Validator
 from utils import isiterable
 
-from typing import Iterable, Never, get_type_hints
+import inspect
+
+from typing import Generic, Iterable, Never, get_type_hints
 
 from openpyxl.cell import Cell
 from pydantic import BaseModel, model_validator
@@ -18,20 +20,29 @@ class RowData(BaseModel, frozen=True, strict=True):
     @model_validator(mode='before')
     @classmethod
     def _input(
-        self, metadata: ColumnMetadata, data: Iterable[Cell]
+        self, obj: tuple[ColumnMetadata, Iterable[Cell]]
     ) -> dict[str, Row] | Never:
+        metadata, data = obj
         assert isiterable(data)
         assert all(isinstance(it, Cell) for it in data)
         assert isinstance(metadata, ColumnMetadata)
         columns, fields = metadata.dict(), get_type_hints(self)
         assert len(fields) == len(data)
         assert len(fields) == len(columns)
-        assert all(prop_here in columns for prop_here in fields.keys())
-        assert all(issubclass(T, Validator) for T in fields.values())
+        for prop_here, T in fields.items():
+            assert prop_here in columns
+            assert issubclass(T, Row) and issubclass(T, Generic)
+            generic_meta = T.__pydantic_generic_metadata__
+            assert generic_meta['origin'] is Row
+            assert len(generic_meta['args']) == 1
+            type_arg = generic_meta['args'][0]
+            assert inspect.isclass(type_arg)
+            assert issubclass(type_arg, Validator)
         validated = {}
         for prop, cell in zip(fields.keys(), data):
-            col = columns['prop']
-            validator: Validator = fields[prop]
+            col = columns[prop]
+            type_metadata = fields[prop].__pydantic_generic_metadata__
+            validator: Validator = type_metadata['args'][0]
             value = validator.validate(cell.value)
             validated[prop] = {
                 'index': col['index'],
