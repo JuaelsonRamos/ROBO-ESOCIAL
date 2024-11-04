f1b1aba975a36ac0c49615261e0787c8de3b46c7
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-04 17:00:40 -0300

    fix(Row): pydantic model propriamente genérico

--- sistema/models/Row.py
+++ sistema/models/Row.py
@@ -2,19 +2,26 @@ from __future__ import annotations
 
 from sistema.models import ColumnMetadata
 from sistema.spreadsheet import QualifiedType, QualifiedValue, Requirement
-from sistema.validators import Validator
+
+from typing import Generic, T
 
 from pydantic import BaseModel
 
 
-class Row(BaseModel, frozen=True, strict=True):
+class Row(
+    BaseModel,
+    Generic[T],
+    frozen=True,
+    strict=True,
+    arbitrary_types_allowed=True,
+):
     index: int
     property_name: str
     required: Requirement
     is_opcional: bool
     is_empty: bool
     qualified_type: QualifiedType
-    validator_class: Validator
+    validator_class: type[T]
     original_value: str
     qualified_value: QualifiedValue
     columns_metadata: ColumnMetadata
