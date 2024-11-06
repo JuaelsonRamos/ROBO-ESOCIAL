23ad7fab1d918e3fbc192943c53b7c14474748b6
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-06 15:20:58 -0300

    feat(utils): add módulo constants, valores utilitários

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
 
 from .assertions import isiterable
+from .constants import INT32_MAX, INT32_MIN, UINT32_MAX
 from .decorators import block, singleton
---
+++ utils/constants.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+
+INT32_MAX = (1 << 31) - 1
+INT32_MIN = -(1 << 31)
+UINT32_MAX = 1 << 32
