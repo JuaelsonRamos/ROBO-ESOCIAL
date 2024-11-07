dd065e6c53b39f05cc7dedfb1b7a6d2b30a47e65
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-07 11:30:13 -0300

    refactor(utils): namespaces de int constantes

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,29 @@
 from __future__ import annotations
 
+from typing import Any, NamedTuple, Self
 
-INT32_MAX = (1 << 31) - 1
-INT32_MIN = -(1 << 31)
-UINT32_MAX = 1 << 32
+
+class _int_maybe_float(int):
+    def __new__(cls, x: Any) -> Self:
+        instance = super().__new__(x)
+        cls.__float_self = float(x)
+        return instance
+
+    def asfloat(self) -> float:
+        return self.__float_self
+
+
+class _integer_namespace(NamedTuple):
+    MIN: _int_maybe_float
+    MAX: _int_maybe_float
+
+
+INT32 = _integer_namespace(
+    MIN=_int_maybe_float(-(2**31)),
+    MAX=_int_maybe_float(2**31 - 1),
+)
+
+UINT32 = _integer_namespace(
+    MIN=_int_maybe_float(0),
+    MAX=_int_maybe_float(2**32),
+)
