b19f91c858b55f75e61f895bdb79f508768fbe2e
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-09 19:08:09 -0300

    feat(validators): add Boolean

---
+++ sistema/validators/boolean.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from .string import String
+from .validator import DefaultDict, ValidatorError
+
+from sistema.models import Column
+
+from typing import Sequence
+
+from openpyxl.cell import Cell
+
+
+class Boolean(String):
+    def __init__(
+        self,
+        *,
+        falsy: Sequence[str] = [],
+        truthy: Sequence[str] = [],
+        case_sensitive: bool = False,
+        expect_unicode: bool = False,
+        allow_empty: bool = False,
+    ):
+        try:
+            assert len(falsy) > 0
+            assert len(truthy) > 0
+            self.falsy, self.truthy = [], []
+            falsy_delta = len(falsy)
+            for i in range(len(falsy) + len(truthy)):
+                value = falsy[i] if i < falsy_delta else truthy[i]
+                assert isinstance(value, str)
+                buffer = self.parse_string(value)
+                assert buffer != ''
+                self.min_string_length = self.max_string_length = len(buffer)
+                (self.falsy if i < falsy_delta else self.truthy).append(buffer)
+            self.falsy = frozenset(self.falsy)
+            self.truthy = frozenset(self.truthy)
+        except AssertionError as err:
+            raise ValidatorError(err) from ValueError(err)
+
+        super().__init__(
+            case_sensitive=case_sensitive,
+            expect_unicode=expect_unicode,
+            allow_empty=allow_empty,
+        )
+
+    def _validate(
+        self, column: Column, cell: Cell, cell_index: int, property_name: str
+    ) -> DefaultDict:
+        namespace = super()._validate(column, cell, cell_index, property_name)
+        if not namespace['is_valid']:
+            return namespace
+        if namespace['is_valid'] and namespace['is_empty']:
+            return namespace
+        value: str = namespace['qualified_value']
+        try:
+            self.validate_string(value)
+            value = self.parse_string(value)
+            if value in self.falsy:
+                namespace['qualified_value'] = False
+            elif value in self.truthy:
+                namespace['qualified_value'] = True
+            else:
+                raise ValueError
+        except (TypeError, ValueError):
+            namespace['qualified_value'] = None
+            namespace['is_valid'] = False
+
+        return namespace
