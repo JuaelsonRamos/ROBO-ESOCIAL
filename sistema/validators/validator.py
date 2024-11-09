100bdb1d1c41b7f30e09bf9cb2c565161a89261d
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-09 11:31:58 -0300

    refactor(Validator): movido para outro módulo

--- sistema/validators/validator.py
+++ sistema/validators/validator.py
@@ -1,16 +1,94 @@
 from __future__ import annotations
 
-from sistema.spreadsheet import QualifiedType, QualifiedValue
+import sistema.spreadsheet as sheet
 
-from abc import ABC, abstractclassmethod
-from re import Pattern
-from typing import Any
+from sistema.models import (
+    Cell as CellModel,
+    Column,
+)
+
+import string
+
+from typing import Any, TypeAlias
+
+from openpyxl.cell import Cell
 
 
-class Validator(ABC):
-    is_regular: bool
-    regex: Pattern[str] | None
-    qualified_type: QualifiedType
+DefaultDict: TypeAlias = dict[str, Any]
 
-    @abstractclassmethod
-    def validate(self, raw_value: Any) -> QualifiedValue | None: ...
+
+class ValidatorError(Exception): ...
+
+
+class CellValidationError(Exception): ...
+
+
+class Validator:
+    _not_inited_var_errmsg = 'propriedade não deve ser acessada antes de inicializada'
+    _inited_var_errmsg = 'propriedade só pode ser inicializada uma única vez'
+
+    def __init__(self) -> None:
+        self.__qualified_type: sheet.QualifiedType | None = None
+        self.__is_arbitrary_string: bool | None = None
+
+    @property
+    def qualified_type(self) -> sheet.QualifiedType:
+        if self.__qualified_type is None:
+            msg = self._not_inited_var_errmsg
+            raise ValidatorError(msg) from AttributeError(msg)
+        return self.__qualified_type
+
+    @qualified_type.setter
+    def qualified_type(self, value: sheet.QualifiedType) -> None:
+        if self.__qualified_type is not None:
+            msg = self._inited_var_errmsg
+            raise ValidatorError(msg) from SyntaxError(msg)
+        self.__qualified_type = value
+
+    @property
+    def is_arbitrary_string(self) -> bool:
+        if self.__is_arbitrary_string is None:
+            msg = self._not_inited_var_errmsg
+            raise ValidatorError(msg) from AttributeError(msg)
+        return self.__is_arbitrary_string
+
+    @is_arbitrary_string.setter
+    def is_arbitrary_string(self, value: bool) -> None:
+        if self.__is_arbitrary_string is not None:
+            msg = self._inited_var_errmsg
+            raise ValidatorError(msg) from SyntaxError(msg)
+        self.__is_arbitrary_string = value
+
+    def _validate(
+        self, column: Column, cell: Cell, cell_index: int, property_name: str
+    ) -> DefaultDict:
+        try:
+            assert isinstance(cell, Cell)
+            assert isinstance(column, Column)
+            assert isinstance(cell_index, int)
+            assert isinstance(property_name, str)
+            assert cell_index >= 0
+            property_name = property_name.strip(string.whitespace)
+            assert property_name != ''
+        except AssertionError as err:
+            raise CellValidationError(err) from err
+
+        return {
+            'index': cell_index,
+            'property_name': property_name,
+            'required': column.required,
+            'is_empty': False,
+            'is_valid': False,
+            'is_arbitrary_string': self.is_arbitrary_string,
+            'qualified_type': self.qualified_type,
+            'validator': self,
+            'original_value': cell.value,
+            'qualified_value': None,
+            'column_metadata': column,
+        }
+
+    def __call__(
+        self, column: Column, cell: Cell, /, cell_index: int, property_name: str
+    ) -> CellModel:
+        namespace = self._validate(column, cell, cell_index, property_name)
+        return CellModel.model_validate(namespace)
