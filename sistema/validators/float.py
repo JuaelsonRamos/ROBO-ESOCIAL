7b5008cbc1d3777857129dd2facd7708c9be58ed
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-09 17:17:59 -0300

    feat(validators): add Float

---
+++ sistema/validators/float.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from .numeric_string import NumericString
+
+import sistema.spreadsheet as sheet
+
+from sistema.models.Column import Column
+from sistema.validators.validator import DefaultDict
+from utils import INT32
+
+from openpyxl.cell.cell import Cell
+
+
+class Float(NumericString):
+    def __init__(
+        self,
+        *,
+        min_value: int | float = INT32.MIN,
+        max_value: int | float = INT32.MAX,
+        allow_zero: bool = True,
+        allow_empty: bool = False,
+    ):
+        super().__init__(
+            min_value=min_value,
+            max_value=max_value,
+            allow_zero=allow_zero,
+            allow_empty=allow_empty,
+        )
+        self._is_arbitrary_string = False
+        self._qualified_type = sheet.FLOAT
+
+    def _validate(
+        self, column: Column, cell: Cell, cell_index: int, property_name: str
+    ) -> DefaultDict:
+        namespace = super()._validate(column, cell, cell_index, property_name)
+        if not namespace['is_valid']:
+            return namespace
+        if namespace['is_valid'] and namespace['is_empty']:
+            return namespace
+        value = namespace['qualified_value']
+        try:
+            # This... should not be necessary at all, but what the hell...
+            value = float(value)
+        except (TypeError, ValueError):
+            namespace['is_valid'] = False
+            namespace['qualified_value'] = None
+        else:
+            namespace['qualified_value'] = value
+
+        return namespace
