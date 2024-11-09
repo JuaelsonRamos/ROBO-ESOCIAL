35c020a6bb3eef764153ec878c4206414dfbf333
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-09 20:52:07 -0300

    feat(validators): add Date

---
+++ sistema/validators/date.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from .string import String
+from .validator import DefaultDict, ValidatorError
+
+import sistema.spreadsheet as sheet
+
+from sistema.models.Column import Column
+
+from datetime import date, datetime
+
+from openpyxl.cell.cell import Cell
+
+
+_MIN_REASONABLE_DATE = date(1970, 1, 1)
+_MAX_REASONABLE_DATE = date(2100, 12, 31)
+_MIN_POSSIBLE_DATE = date(1, 1, 1)
+
+
+class Date(String):
+    min_possible_date = _MIN_POSSIBLE_DATE
+    min_reasonable_date = _MIN_REASONABLE_DATE
+    max_reasonable_date = _MAX_REASONABLE_DATE
+
+    def __init__(
+        self,
+        *,
+        min_date: date = _MIN_REASONABLE_DATE,
+        max_date: date = _MAX_REASONABLE_DATE,
+        allow_empty: bool = False,
+    ):
+        self._is_arbitrary_string = False
+        self._qualified_type = sheet.DATE
+        self.format_length: int = len('dd/mm/aaaa')
+        super().__init__(
+            min_string_length=self.format_length,
+            max_string_length=self.format_length,
+            case_sensitive=False,
+            expect_unicode=False,
+            allow_empty=allow_empty,
+        )
+
+        try:
+            assert min_date >= self.min_reasonable_date
+            assert max_date <= self.max_reasonable_date
+            assert min_date < max_date
+        except AssertionError as err:
+            raise ValidatorError(err) from ValueError(err)
+
+        self.format = '%d/%m/%Y'
+        self.min_date = min_date
+        self.max_date = max_date
+
+    def _validate(
+        self, column: Column, cell: Cell, cell_index: int, property_name: str
+    ) -> DefaultDict:
+        namespace = super()._validate(column, cell, cell_index, property_name)
+        if not namespace['is_valid']:
+            return namespace
+        if namespace['is_valid'] and namespace['is_empty']:
+            return namespace
+        value: str = namespace['qualified_value']
+        try:
+            t = datetime.strptime(value, self.format).date()
+            assert t >= self.min_date
+            assert t <= self.max_date
+            namespace['qualified_value'] = t
+        except (AssertionError, TypeError, ValueError):
+            namespace['is_valid'] = False
+            namespace['qualified_value'] = None
+
+        return namespace
