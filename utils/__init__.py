f9bf2899a0f0fe8b77c1a5b2fbbda5e69b469b19
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-11 15:44:44 -0300

    feat(utils): add módulo exceptions, add exception EmptyString

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from __future__ import annotations
 from .assertions import isiterable
 from .constants import INT32, UINT32
 from .decorators import block, singleton
+from .exceptions import EmptyString
