39d383cbea6a43d55c75d332eb700781f0175e81
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-12 12:10:47 -0300

    fix(Modelo1): classe não é singleton

--- sistema/parsers/modelo1/parser.py
+++ sistema/parsers/modelo1/parser.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 from .schema import schema
 from ..parser import Parser
 
+from utils import singleton
 
+
+@singleton
 class Modelo1(Parser):
     def _define_schema(self):
         super()._define_schema()
