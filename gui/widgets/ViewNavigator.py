87070c8c593277ad70539036dfae8f3ba89f7bdd
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-21 17:29:12 -0300

    fix(ViewNavigator): typing, state setting/inquiring, active setting

--- gui/widgets/ViewNavigator.py
+++ gui/widgets/ViewNavigator.py
@@ -7,40 +7,40 @@ import tkinter.ttk as ttk
 
 
 class ViewButton(ttk.Button):
-    def __init__(self, master: ttk.Widget, text: str, *, index: int):
+    def __init__(self, master: ViewNavigator, text: str, *, index: int):
         super().__init__(master, text=text, style=const.VIEW_BUTTON, command=self.click)
         self.pack(fill=tk.X, side=tk.TOP, ipady=4)
         self.index = index
 
     def click(self):
-        self.master.switch_view(self.index)
+        self.master.event_generate('<<SwitchView>>', state=self.index)
 
     def active(self):
-        if self.state == const.PRESSED:
+        if self.instate([const.PRESSED]):
             return
-        self.state = const.PRESSED
+        self.state([const.PRESSED])
         self.config(style=const.ACTIVE_VIEW_BUTTON)
 
     def disabled(self):
-        if self.state == const.NOTPRESSED:
+        if self.instate([const.NOTPRESSED]):
             return
-        self.state = const.NOTPRESSED
+        self.state([const.NOTPRESSED])
         self.config(style=const.VIEW_BUTTON)
 
 
 class ViewNavigator(ttk.Frame):
-    _buttons: list[tk.Widget] = []
+    _buttons: list[ViewButton] = []
 
     def __init__(self, master):
         super().__init__(master, style=const.VIEW_NAVIGATOR)
         self.pack(side=tk.LEFT, anchor=tk.W, fill=tk.Y)
+        self.bind('<<SwitchView>>', self.switch_view)
 
-    def add_button(self, text: str = 'Placeholder', *, active=False):
-        index = len(self._buttons)
-        self._buttons.append(ViewButton(self, text, index=index))
-        if active:
-            self.switch_view(index)
+    def add_button(self, text: str = 'Placeholder'):
+        btn = ViewButton(self, text, index=len(self._buttons))
+        self._buttons.append(btn)
+        return btn
 
-    def switch_view(self, index: int):
+    def switch_view(self, event: tk.Event):
         for btn in self._buttons:
-            btn.active() if btn.index == index else btn.disabled()
+            btn.active() if btn.index == event.state else btn.disabled()
