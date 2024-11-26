82d0aa9a1a2e4b08986aa0d3f098af4cfab1af88
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-26 11:06:20 -0300

    feat(windows): returnar apenas namespaces sobre monitores

--- src/windows.py
+++ src/windows.py
@@ -1,24 +1,31 @@
 from __future__ import annotations
 
+from collections import namedtuple
+from types import SimpleNamespace
+
 import win32api as api
 import win32con as con
+
+
+_Monitor = namedtuple('_Monitor', ('MonitorInfo', 'DisplaySettings'))
 
 
 def get_monitor_settings():
     """Retorna `Iterable` de informações sobre os monitores conectados.
 
-    :return: tuple de tuples no formato: (win32api._MonitorInfo, win32api.PyDEVMODEW)
+    :return: tuple de namedtuples no formato: (win32api._MonitorInfo, win32api.PyDEVMODEW)
     """
     monitors = [api.GetMonitorInfo(info[0]) for info in api.EnumDisplayMonitors()]
     as_devices = [
         api.EnumDisplaySettings(info['Device'], con.ENUM_CURRENT_SETTINGS)
         for info in monitors
     ]
-    return tuple((mon, dev) for mon, dev in zip(monitors, as_devices))
+    return tuple(
+        _Monitor(SimpleNamespace(**mon), dev) for mon, dev in zip(monitors, as_devices)
+    )
 
 
 def get_primary_monitor():
-    monitors = [api.GetMonitorInfo(info[0]) for info in api.EnumDisplayMonitors()]
-    for monitor in monitors:
-        if monitor.Flag == con.MONITORINFOF_PRIMARY:
+    for monitor in get_monitor_settings():
+        if monitor.MonitorInfo.Flags | con.MONITORINFOF_PRIMARY:
             return monitor
