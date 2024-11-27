c7c9df7ca931d76d74035b3df90b0dfe622e0024
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-27 11:39:58 -0300

    refactor(utils): 'singleton' metaclass, 'value' decorator, remove tests

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,27 +1,19 @@
 from __future__ import annotations
 
-import abc
-import sys
-import inspect
-
-from typing import Any, Callable, TypeVar, cast
+from typing import Any, Callable, TypeVar
 
 
-C = TypeVar('C', bound=Any)
+T = TypeVar('T', bound=Any)
 
 
 def block(function: Callable[[], None]) -> None:
-    """Decorador de transforma uma definição de função em um
-    bloco de código anônimo. A função não será definida e
-    será imediatamente executada."""
+    """
+    Decorador de transforma uma definição de função em um bloco de código anônimo.
+
+    A função não será definida e será imediatamente executada.
+    """
     function()
 
-
-def singleton(cls: type[C]) -> C:
-    assert not inspect.isbuiltin(cls)
-    assert inspect.getmodule(cls) is not sys.modules['builtins']
-    assert inspect.isclass(cls)
-    assert abc.ABCMeta not in cls.__bases__
-    assert abc.ABC not in cls.__bases__
-    assert callable(cls)
-    return cast(C, cls())
+def value(func: Callable[[], T]) -> T:
+    """Decorador que transforma função em variável com valor igual ao retornado."""
+    return func()
---
+++ src/utils/objects.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+class Singleton(type):
+    __slots__ = ('__instances')
+    __instances: dict[type, Singleton] = {}
+
+    def __call__(cls) -> Singleton:
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(Singleton, cls).__call__()
+        return cls.__instances[cls]
