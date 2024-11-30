c652eb825da24b4b5202e5385e6a1c8dc91868d8
Marcos Bião <dev.marcosdly@gmail.com>
2024-11-30 16:53:09 -0300

    feat(windows): add constant APPDATA_DIR

--- src/windows.py
+++ src/windows.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import os
+
 from collections import namedtuple
+from pathlib import Path
 from types import SimpleNamespace
+from typing import Final, cast
 
 import win32api as api
 import win32con as con
@@ -9,13 +13,23 @@ import win32con as con
 
 _Monitor = namedtuple('_Monitor', ('MonitorInfo', 'DisplaySettings'))
 
+APPDATA_DIR: Final[Path] = (
+    Path('./__debug__/LocalAppData')
+    if __debug__
+    else Path(os.environ['LOCALAPPDATA']) / 'RoboEsocial'
+)
+
 
 def get_monitor_settings():
-    """Retorna `Iterable` de informações sobre os monitores conectados.
-
-    :return: tuple de namedtuples no formato: (win32api._MonitorInfo, win32api.PyDEVMODEW)
     """
-    monitors = [api.GetMonitorInfo(info[0]) for info in api.EnumDisplayMonitors()]
+    Retorna `Iterable` de informações sobre os monitores conectados.
+
+    :return: tuple de namedtuples no formato: (win32api._MonitorInfo,
+        win32api.PyDEVMODEW)
+    """
+    monitors = [
+        api.GetMonitorInfo(cast(int, info[0])) for info in api.EnumDisplayMonitors()
+    ]
     as_devices = [
         api.EnumDisplaySettings(info['Device'], con.ENUM_CURRENT_SETTINGS)
         for info in monitors
