1037660edbc8f64b4e39e176119fb04d49066cfc
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-03 12:38:14 -0300

    feat(gui): ViewNavigator: defining and switching views

--- src/gui/widgets/ViewNavigator.py
+++ src/gui/widgets/ViewNavigator.py
@@ -2,45 +2,72 @@ from __future__ import annotations
 
 import src.gui.constants as const
 
+from src.gui.views.View import View
+
 import tkinter as tk
 import tkinter.ttk as ttk
 
+from typing import cast
+
 
 class ViewButton(ttk.Button):
-    def __init__(self, master: ViewNavigator, text: str, *, index: int):
+    def __init__(
+        self,
+        master: ViewNavigator,
+        view: View | None = None,
+        text: str = '',
+        *,
+        index: int,
+    ):
         super().__init__(master, text=text, style=const.VIEW_BUTTON, command=self.click)
         self.pack(fill=tk.X, side=tk.TOP, ipady=4)
         self.index = index
+        self.view = view
+        self._pressed: bool = False
+        if view is None:
+            self.config(style=const.VIEW_BUTTON)
 
     def click(self):
+        if self.view is None:
+            return
         self.master.event_generate('<<SwitchView>>', state=self.index)
 
     def active(self):
-        if self.instate([const.PRESSED]):
+        if self.view is None or self._pressed:
             return
-        self.state([const.PRESSED])
+        self._pressed = True
         self.config(style=const.ACTIVE_VIEW_BUTTON)
 
     def disabled(self):
-        if self.instate([const.NOTPRESSED]):
+        if self.view is None or not self._pressed:
             return
-        self.state([const.NOTPRESSED])
+        self._pressed = False
         self.config(style=const.VIEW_BUTTON)
 
 
 class ViewNavigator(ttk.Frame):
     _buttons: list[ViewButton] = []
+    _active: int | None = None
 
     def __init__(self, master):
         super().__init__(master, style=const.VIEW_NAVIGATOR)
         self.pack(side=tk.LEFT, anchor=tk.W, fill=tk.Y)
         self.bind('<<SwitchView>>', self.switch_view)
 
-    def add_button(self, text: str = 'Placeholder') -> ViewButton:
-        btn = ViewButton(self, text, index=len(self._buttons))
+    def add_button(self, text: str = '', widget: View | None = None) -> ViewButton:
+        btn = ViewButton(self, widget, text, index=len(self._buttons))
         self._buttons.append(btn)
         return btn
 
     def switch_view(self, event: tk.Event) -> None:
+        if self._active == event.state:
+            return
         for btn in self._buttons:
             btn.active() if btn.index == event.state else btn.disabled()
+        previous, current = self._active, cast(int, event.state)
+        if previous is not None:
+            btn = self._buttons[previous]
+            cast(View, btn.view).hide()
+        btn = self._buttons[current]
+        cast(View, btn.view).pack()
+        self._active = current
