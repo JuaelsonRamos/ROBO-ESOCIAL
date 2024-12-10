07195dfa992eb944e99388e3d9dee8ebc341a1af
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-10 12:54:40 -0300

    fix(gui): GlobalRuntimeConstants: inexistent private properties

--- src/gui/global_runtime_constants.py
+++ src/gui/global_runtime_constants.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import asdict, astuple, dataclass
 from tkinter import ttk
-from typing import Any, Never
+from typing import Any, Never, cast
 
 
 class _InitializableSingletonMeta(type):
@@ -21,71 +21,66 @@ class _InitializableSingletonMeta(type):
     """
 
     __slots__ = ()
-    __instance: GlobalRuntimeConstants | None = None
-    __can_init: bool = False
-    __not_inited_err: str = (
+    _instance: GlobalRuntimeConstants | None = None
+    _can_init: bool = False
+    _not_inited_err: str = (
         'instance of GlobalRuntimeConstants not yet initialized, call .configure() once'
     )
-    __astuple_cache = None
-    __asdict_cache = None
+    _astuple_cache = None
+    _asdict_cache = None
 
     def __call__(cls, *args, **kwargs) -> GlobalRuntimeConstants | Never:
-        if cls.__instance is not None:
-            return cls.__instance
-        if not cls.__can_init:
+        if cls._instance is not None:
+            return cls._instance
+        if not cls._can_init:
             raise RuntimeError(
                 'cannot init class by calling it directly, use the .configure() method instead'
             )
-        cls.__instance = super().__call__(*args, **kwargs)
-        return cls.__instance  # type: ignore
+        try:
+            cls._instance = super().__call__(*args, **kwargs)
+            return cls._instance  # type: ignore
+        finally:
+            cls._can_init = False
 
     @property
-    def configure(cls) -> GlobalRuntimeConstants | Never:
-        if cls.__instance is not None:
+    def configure(cls) -> type[GlobalRuntimeConstants] | Never:
+        if cls._instance is not None:
             raise RuntimeError(
                 'only allowed instance already initialized, call the class/object to retrieve it'
             )
-        try:
-            cls.__can_init = True
-            return cls  # type: ignore
-        finally:
-            cls.__can_init = False
+        cls._can_init = True
+        return cls  # type: ignore
 
     @property
     def __getitem__(cls):
         return cls.__getattribute__
 
-    def __getattribute__(cls, name: str) -> Any | Never:
-        if cls.__instance is None:
-            raise RuntimeError(cls.__not_inited_err)
-        return super().__getattribute__(name)  # type: ignore
-
     def astuple(cls) -> tuple[Any, ...] | Never:
-        if cls.__instance is None:
-            raise RuntimeError(cls.__not_inited_err)
+        if cls._instance is None:
+            raise RuntimeError(cls._not_inited_err)
         if cls.__astuple_cache is None:
-            cls.__astuple_cache = astuple(cls.__instance)
+            cls.__astuple_cache = astuple(cls._instance)
         return cls.__astuple_cache
 
     def asdict(cls) -> dict[str, Any] | Never:
-        if cls.__instance is None:
-            raise RuntimeError(cls.__not_inited_err)
+        if cls._instance is None:
+            raise RuntimeError(cls._not_inited_err)
         if cls.__asdict_cache is None:
-            cls.__asdict_cache = asdict(cls.__instance)
+            cls.__asdict_cache = asdict(cls._instance)
         return cls.__asdict_cache
 
     def is_initialized(cls) -> bool:
         """Whether the dataclass (singleton) has been initialized."""
-        return cls.__instance is not None
+        return cls._instance is not None
 
     def try_get(cls, attr: str, default: Any = None) -> Any:
         """Returns value of attribute IF AND ONLY IF dataclass has been initialized;
         otherwise, return None.
         """
-        if cls.__instance is None:
+        if cls._instance is None:
             return default
         try:
-            return getattr(cls.__instance, attr, default)
+            return getattr(cls._instance, attr, default)
         except RuntimeError:
             return default
 
