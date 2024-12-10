7f660b6ffc80b6065aeb5cb167a70da9a8339a9c
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-10 20:32:42 -0300

    feat(mainloop): self contained application event loop contexts

--- src/mainloop.py
+++ src/mainloop.py
@@ -5,81 +5,194 @@ from src.crawl.steps.step import execute_in_order
 from src.gui.app import App
 from src.gui.global_runtime_constants import GlobalRuntimeConstants
 
+import sys
 import asyncio
-import functools
-import itertools
+import argparse
 
 from pathlib import Path
+from tkinter import ttk
+from types import NoneType
+from typing import Any, Final
 
-from playwright.async_api import async_playwright
+from playwright.async_api import (
+    Browser,
+    BrowserContext,
+    Page,
+    Playwright,
+    async_playwright,
+)
 from sqlalchemy import create_engine
 
 
 TASK_LIMIT = 5
 
-task_counter = itertools.count(1)
-
-default_step_order: tuple[str, ...] = tuple()
 
 app_dir = bootstrap.Directory()
 
 
-def get_firefox_exe() -> Path | None:
-    path: Path | None = None
-    try:
-        gen = app_dir.PLAYWRIGHT.glob('firefox-*/firefox/firefox.exe')
-        path = next(gen)
-        gen.close()
-    except (GeneratorExit, StopIteration):
+def parse_cli_args():
+    parser = argparse.ArgumentParser(
+        allow_abbrev=False,
+        prog='ROBO-ESOCIAL',
+        argument_default=None,
+        exit_on_error=False,
+        add_help=True,
+    )
+    parser.add_argument('--no-playwright', action='store_true')
+    parser.add_argument('--no-tkinter', action='store_true')
+    args = parser.parse_args(sys.argv[1:] if __debug__ else [])
+    if args.no_playwright and args.no_tkinter:
+        raise RuntimeError(
+            'cannot specify both arguments --no-tkinter and --no-playwright'
+        )
+    return args
+
+
+CLI_ARGS: Final[argparse.Namespace] = parse_cli_args()
+
+
+class BrowserRuntime:
+    default_step_order: tuple[str, ...] = ()
+
+    def __init__(self, p: Playwright) -> None:
+        super().__init__()
+        self.p = p
+        self.firefox_exe = self.get_firefox_exe()
+        self._browser = None
+        self._context = None
+        self.semaphore = asyncio.Semaphore(TASK_LIMIT)
+        self.queue = asyncio.Queue()
+        self.task_i: int = 1
+
+    @classmethod
+    def should_avoid(cls) -> bool:
+        global CLI_ARGS
+        return getattr(CLI_ARGS, 'no_playwright', False)
+
+    @classmethod
+    async def single_isolated_runtime_loop(cls) -> None:
+        async with async_playwright() as p:
+            instance = cls(p)
+            while True:
+                await instance.create_task(raise_if_cant=False)
+
+    def can_create_task(self) -> bool:
+        return not (self.queue.empty() or self.semaphore.locked())
+
+    def _on_task_done(self, _: asyncio.Task[None]) -> None:
+        self.semaphore.release()
+
+    def get_firefox_exe(self) -> Path | None:
+        path: Path | None = None
+        try:
+            gen = app_dir.PLAYWRIGHT.glob('firefox-*/firefox/firefox.exe')
+            path = next(gen)
+            gen.close()
+        except (GeneratorExit, StopIteration):
+            pass
+        return path
+
+    class CannotCreateTaskError(RuntimeError):
         pass
-    return path
+
+    async def create_task(self, *, raise_if_cant: bool) -> asyncio.Task[None] | None:
+        if self.queue.empty() or self.semaphore.locked():
+            # can't create task, awaiting semaphore would halt coroutine indefinitely
+            if not raise_if_cant:
+                # consumer doesn't want it to raise on error
+                return
+            raise self.CannotCreateTaskError(
+                'either queue is empty or semaphore is closed'
+            )
+        await self.semaphore.acquire()
+        index_string = str(self.task_i).ljust(3, '0')
+        name = f'crawler({index_string})'
+        browser = self._browser or await self.browser()
+        context = self._context or await self.context()
+        page = await self.new_page()
+        coro = execute_in_order(browser, context, page, self.default_step_order)
+        task = asyncio.create_task(coro, name=name)
+        task.add_done_callback(self._on_task_done)
+        self.task_i += 1
+        return task
+
+    async def browser(self) -> Browser:
+        if self._browser is None:
+            self._browser = await self.p.firefox.launch(
+                executable_path=self.firefox_exe, headless=False
+            )
+        return self._browser
+
+    async def context(self) -> BrowserContext:
+        browser = self._browser or await self.browser()
+        if self._context is None:
+            self._context = await browser.new_context()
+        return self._context
+
+    async def new_page(self) -> Page:
+        context = self._context or await self.context()
+        return await context.new_page()
+
+
+class GraphicalRuntime:
+    def __init__(self) -> NoneType:
+        super().__init__()
+        self.app = App()
+        db.populate_backup_files()
+        self.sqlite_engine = create_engine(
+            'sqlite://',
+            creator=db.define_connection,
+            echo=__debug__,
+            echo_pool=__debug__,
+        )
+        GlobalRuntimeConstants.configure(
+            style=ttk.Style(self.app),
+            sqlite=self.sqlite_engine,
+        )
+        self.global_constants = GlobalRuntimeConstants()  # type: ignore
+
+        import src.gui.asyncio as gui_asyncio
+
+        self.gui_asyncio_helpers = gui_asyncio
+
+    def stop_all_threads(self):
+        self.gui_asyncio_helpers.Thread.stop_all()
+
+    @classmethod
+    def should_avoid(cls) -> bool:
+        global CLI_ARGS
+        return getattr(CLI_ARGS, 'no_tkinter', False)
+
+    @classmethod
+    def single_isolated_runtime_loop(cls) -> None:
+        instance = cls()
+        instance.app.mainloop()
+
+
+async def production_runtime_loop(browser: BrowserRuntime, gui: GraphicalRuntime):
+    try:
+        while await asyncio.sleep(gui.app.frametime, True):
+            gui.app.tick()
+            if gui.app.has_quit():
+                break
+            while browser.can_create_task():
+                await browser.create_task(raise_if_cant=True)
+    finally:
+        gui.stop_all_threads()
 
 
 async def mainloop():
     if not app_dir.is_ensured():
         app_dir.ensure_mkdir()
 
+    if BrowserRuntime.should_avoid():
+        GraphicalRuntime.single_isolated_runtime_loop()
+        return
+    if GraphicalRuntime.should_avoid():
+        await BrowserRuntime.single_isolated_runtime_loop()
+        return
+
     async with async_playwright() as p:
-        firefox_exe = get_firefox_exe()
-        browser = await p.firefox.launch(executable_path=firefox_exe, headless=False)
-        context = await browser.new_context()
-        task_sem = asyncio.Semaphore(TASK_LIMIT)
-        task_queue = asyncio.Queue()
-        new_page = functools.partial(context.new_page)
-
-        def task_can_create() -> bool:
-            return not (task_queue.empty() or task_sem.locked())
-
-        def release_semaphore(*_) -> None:
-            nonlocal task_sem
-            task_sem.release()
-
-        app = App()
-
-        db.populate_backup_files()
-        sqlite_engine = create_engine(
-            'sqlite://',
-            creator=db.define_connection,
-            echo=__debug__,
-            echo_pool=__debug__,
-        )
-
-        GlobalRuntimeConstants.configure(style=ttk.Style(app), sqlite=sqlite_engine)
-
-        import src.gui.asyncio as gui_asyncio
-
-        while await asyncio.sleep(app.frametime, True):
-            app.tick()
-            if app.has_quit():
-                break
-            while task_can_create():
-                await task_sem.acquire()
-                task_i = str(next(task_counter)).ljust(3, '0')
-                task_name = f'crawler({task_i})'
-                task_coro = execute_in_order(
-                    browser, context, await new_page(), default_step_order
-                )
-                task = asyncio.create_task(task_coro, name=task_name)
-                task.add_done_callback(release_semaphore)
-
-        gui_asyncio.Thread.stop_all()
+        browser_runtime = BrowserRuntime(p)
+        graphical_runtime = GraphicalRuntime()
+        await production_runtime_loop(browser_runtime, graphical_runtime)
