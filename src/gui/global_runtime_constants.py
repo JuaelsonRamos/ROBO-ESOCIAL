e7da77da8b7ec24ad390a908d7f47d57c0d17dd8
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-17 12:41:41 -0300

    refactor(gui): GlobalRuntimeConstants: simplify, add exceptions

--- src/gui/global_runtime_constants.py
+++ src/gui/global_runtime_constants.py
@@ -1,107 +1,55 @@
 from __future__ import annotations
 
-from dataclasses import asdict, astuple, dataclass
 from tkinter import ttk
-from typing import Any, Never, cast
+from typing import Any, Final, Generator
 
 import sqlalchemy
 
 
-class _InitializableSingletonMeta(type):
-    """
-    Allows dataclass' __init__ to be called once by calling the .configure() method only
-    once, then call the class/object to retrieve the same instance.
-
-    The following actions will raise :ref:`RuntimeError`:
-
-    * Calling the class/object before .configure() will raise RuntimeError.
-    * Calling .configure() more than once will raise RuntimeError.
-    * Calling custom methods (a.k.a doesn't exist in the base dataclass object)
-        before calling .configure() may raise RuntimeError.
-
-    Please check for methods returning :ref:`Never`, as they are the ones that may raise.
-    """
-
-    __slots__ = ()
-    _instance: GlobalRuntimeConstants | None = None
-    _can_init: bool = False
-    _not_inited_err: str = (
-        'instance of GlobalRuntimeConstants not yet initialized, call .configure() once'
-    )
-    _astuple_cache = None
-    _asdict_cache = None
-
-    def __call__(cls, *args, **kwargs) -> GlobalRuntimeConstants | Never:
-        if cls._instance is not None:
-            return cls._instance
-        if not cls._can_init:
-            raise RuntimeError(
-                'cannot init class by calling it directly, use the .configure() method instead'
-            )
-        try:
-            cls._instance = super().__call__(*args, **kwargs)
-            return cls._instance  # type: ignore
-        finally:
-            cls._can_init = False
-
-    @property
-    def configure(cls) -> type[GlobalRuntimeConstants] | Never:
-        if cls._instance is not None:
-            raise RuntimeError(
-                'only allowed instance already initialized, call the class/object to retrieve it'
-            )
-        cls._can_init = True
-        return cls  # type: ignore
-
-    @property
-    def __getitem__(cls):
-        return cls.__getattribute__
-
-    def astuple(cls) -> tuple[Any, ...] | Never:
-        if cls._instance is None:
-            raise RuntimeError(cls._not_inited_err)
-        if cls.__astuple_cache is None:
-            cls.__astuple_cache = astuple(cls._instance)
-        return cls.__astuple_cache
-
-    def asdict(cls) -> dict[str, Any] | Never:
-        if cls._instance is None:
-            raise RuntimeError(cls._not_inited_err)
-        if cls.__asdict_cache is None:
-            cls.__asdict_cache = asdict(cls._instance)
-        return cls.__asdict_cache
-
-    def is_initialized(cls) -> bool:
-        """Whether the dataclass (singleton) has been initialized."""
-        return cls._instance is not None
-
-    def try_get(cls, attr: str, default: Any = None) -> Any:
-        """Returns value of attribute IF AND ONLY IF dataclass has been initialized;
-        otherwise, return None.
-        """
-        if cls._instance is None:
-            return default
-        try:
-            return getattr(cls._instance, attr, default)
-        except RuntimeError:
-            return default
+class TkinterGlobalUndefined(NotImplementedError):
+    def __init__(self, name: str, /) -> None:
+        super().__init__(f'property {name=} has not yet been assigned')
 
 
-class _InitializableSingleton(_InitializableSingletonMeta):
-    """Dummy class to prevent metaclass' __call__ method from confusing the static type
-    checker.
-    """
+class TkinterGlobalAssigned(RuntimeError):
+    def __init__(self, name: str, /) -> None:
+        super().__init__(f'property {name=} has already been assigned once')
 
 
-@dataclass(frozen=True, slots=True, kw_only=True)
-class GlobalRuntimeConstants(metaclass=_InitializableSingleton):
-    """
-    Dataclass holding constant references to values expected to be used throught the
-    application as global values.
-
-    All values must be initialized, and to avoid bugs, preferably before any user action
-    is performed.
-    """
+class TkinterGlobalType:
+    __slots__ = ('style', 'sqlite')
 
     style: ttk.Style
     sqlite: sqlalchemy.Engine
+
+    def __getattr__(self, name: str, /) -> Any:
+        if getattr(super(), name, None) is None:
+            raise TkinterGlobalUndefined(name)
+        return getattr(super(), name)
+
+    def __getitem__(self, key: str, /) -> Any:
+        return self.__getattr__(key)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if hasattr(self, name) or getattr(super(), name) is not None:
+            raise TkinterGlobalAssigned(name)
+
+    def fields(self) -> tuple[str, ...]:
+        return self.__slots__
+
+    def items(self) -> Generator[tuple[str, Any | None], None, None]:
+        for prop in self.__slots__:
+            yield (prop, getattr(super(), prop, None))
+
+    def has(self, name: str, /) -> bool:
+        """Has attr and is initialized."""
+        try:
+            if getattr(super(), name) is None:
+                return False
+        except AttributeError:
+            return False
+        else:
+            return True
+
+
+TkinterGlobal: Final[TkinterGlobalType] = TkinterGlobalType()
