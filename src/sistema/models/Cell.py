1eb6f20fb23b2c4f9b79ec206b74eb3e83fc61e4
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-20 15:56:21 -0300

    chore(models): add generic T_CellValue to Cell model

--- src/sistema/models/Cell.py
+++ src/sistema/models/Cell.py
@@ -4,16 +4,18 @@ from .Column import Column
 from .Model import model
 
 from src.sistema.validators import Validator
-from src.types import CellValue, IsRequired
+from src.types import IsRequired, T_CellValue
+
+from typing import Generic
 
 
 @model
-class Cell:
+class Cell(Generic[T_CellValue]):
     index: int
     required: IsRequired
     is_empty: bool
     is_valid: bool
     validator: Validator
     original_value: str | None
-    parsed_value: CellValue | None
+    parsed_value: T_CellValue | None
     column_metadata: Column
