172b2161a70388b941c0e3246ce0723fa9f6892c
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-24 18:43:56 -0300

    chore: remove src.utils package

--- src/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .assertions import isiterable
-from .constants import INT32, UINT32
-from .decorators import block, value
-from .exceptions import EmptyString
-from .iterables import extract_from
-from .objects import Singleton
