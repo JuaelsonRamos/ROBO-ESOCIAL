6941f99df76fa8501dfc58f12b95790d5f7d7b2e
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-25 21:43:17 -0300

    feat(mainloop): remove previous cli argument parser

--- src/mainloop.py
+++ src/mainloop.py
@@ -4,41 +4,20 @@ from src.bootstrap import Directory
 from src.db import init_sync_sqlite
 from src.gui.app import App
 from src.gui.tkinter_global import TkinterGlobal
+from src.runtime import CommandLineArguments
 from src.sistema.crawl import BrowserRuntime
 
-import sys
 import asyncio
-import argparse
 
 from tkinter import ttk
 from types import NoneType
-from typing import Final
 
 from playwright.async_api import async_playwright
 
 
-def parse_cli_args():
-    parser = argparse.ArgumentParser(
-        allow_abbrev=False,
-        prog='ROBO-ESOCIAL',
-        argument_default=None,
-        exit_on_error=False,
-        add_help=True,
-    )
-    parser.add_argument('--no-playwright', action='store_true')
-    parser.add_argument('--no-tkinter', action='store_true')
-    args = parser.parse_args(sys.argv[1:] if __debug__ else [])
-    if args.no_playwright and args.no_tkinter:
-        raise RuntimeError(
-            'cannot specify both arguments --no-tkinter and --no-playwright'
-        )
-    return args
-
-
-CLI_ARGS: Final[argparse.Namespace] = parse_cli_args()
-
-
 class GraphicalRuntime:
+    cli_args = CommandLineArguments().parse_argv()
+
     def __init__(self) -> NoneType:
         super().__init__()
         self.app = App()
@@ -47,8 +26,7 @@ class GraphicalRuntime:
 
     @classmethod
     def should_avoid(cls) -> bool:
-        global CLI_ARGS
-        return getattr(CLI_ARGS, 'no_tkinter', False)
+        return cls.cli_args.no_tkinter
 
     @classmethod
     def single_isolated_runtime_loop(cls) -> None:
