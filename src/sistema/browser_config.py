df74f44f7aaf75cae60c2a4f0029bf8ea742205e
Marcos Bião <dev.marcosdly@gmail.com>
2024-12-26 10:35:52 -0300

    feat(sistema): browser_config: firefox preferences, policies and documentation

--- src/sistema/browser_config.py
+++ src/sistema/browser_config.py
@@ -4,17 +4,137 @@ import io
 import json
 import hashlib
 
-from typing import Literal
+from typing import Any, Literal
 
 
 class FirefoxConfig:
+    # documentation
+    # installer config https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
+    # about: links https://kb.mozillazine.org/About_protocol_links
     encoding = 'utf-8'
     override_ini: dict[str, dict[str, str | float | bool | int]] = {
         'App': {'Vendor': 'Mozilla', 'Name': 'Firefox'},
         'XRE': {'EnableProfileMigrator': False},
     }
-    policies_json: dict = {}
-    user_js: dict[str, str | float | bool] = {}
+    policies_json: dict[str, Any] = {
+        'policies': {
+            'AppAutoUpdate': False,
+            'AutofillAddressEnabled': False,
+            'AutofillCreditCardEnabled': False,
+            'BackgroundAppUpdate': False,
+            'BlockAboutAddons': not __debug__,
+            'BlockAboutConfig': not __debug__,
+            'BlockAboutProfiles': not __debug__,
+            'BlockAboutSupport': not __debug__,
+            'DisableAppUpdate': True,
+            'DisableDeveloperTools': not __debug__,
+            'DisableFeedbackCommands': not __debug__,
+            'DisableFirefoxAccounts': not __debug__,
+            'DisableFirefoxStudies': True,
+            'DisableForgetButton': True,
+            'DisableFormHistory': not __debug__,
+            'DisablePasswordReveal': not __debug__,
+            'DisablePocket': True,
+            'DisablePrivateBrowsing': not __debug__,
+            'DisableProfileImport': True,
+            'DisableProfileRefresh': not __debug__,
+            'DisableSafeMode': not __debug__,
+            'DisableSecurityBypass': {
+                'InvalidCertificate': True,
+                'SafeBrowsing': True,
+            },
+            'DisableSetDesktopBackground': True,
+            'DisableSystemAddonUpdate': True,
+            'DisableTelemetry': True,
+            'DisplayBookmarksToolbar': 'never' if not __debug__ else 'always',
+            'DisplayMenuBar': 'never' if not __debug__ else 'always',
+            'DontCheckDefaultBrowser': True,
+            'EnableTrackingProtection': {
+                'Lock': True,
+                'Cryptomining': True,
+                'EmailTracking': True,
+            },
+            'EncryptedMediaExtensions': {
+                'Enabled': True,
+                'Locked': True,
+            },
+            'FirefoxSuggest': {
+                'WebSuggestions': False,
+                'SponsoredSuggestions': False,
+                'ImproveSuggest': False,
+                'Locked': True,
+            },
+            'HardwareAcceleration': True,
+            'NoDefaultBookmarks': True,
+            'OfferToSaveLogins': False,
+            'OfferToSaveLoginsDefault': False,
+            'OverrideFirstRunPage': '',  # not displayerd
+            'OverridePostUpdatePage': '',  # not displayed
+            'PasswordManagerEnabled': __debug__,
+            'Permissions': {
+                'Camera': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+                'Microphone': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+                'Notifications': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+                'Autoplay': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+                'VirtualReality': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+                'Location': {
+                    'Locked': not __debug__,
+                    'BlockNewRequests': True,
+                },
+            },
+            'PictureInPicture': {
+                'Locked': True,
+                'Enabled': False,
+            },
+            # 'PopupBlocking': {},  # TODO consider blocking
+            'PrimaryPassword': False,
+            'PrivateBrowsingModeAvailability': 1,  # not available
+            'SanitizeOnShutdown': {
+                'FormData': True,
+                'Locked': not __debug__,
+            },
+            'SearchBar': 'separate',  # not displayed
+            'SearchSuggestEnabled': __debug__,
+            'ShowHomeButton': False,
+            'StartDownloadsInTempDirectory': False,
+            'SupportMenu': False,
+            'TranslateEnabled': False,
+            'UserMessaging': {
+                'Locked': True,
+                'ExtensionRecommendations': False,
+                'FeatureRecommendations': False,
+                'UrlbarInterventions': False,
+                'SkipOnboarding': True,
+                'MoreFromMozilla': False,
+                'FirefoxLabs': False,
+            },
+            'WindowsSSO': False,
+        }
+    }
+    user_js: dict[str, str | float | bool] = {
+        'prefs.converted-to-utf8': True,
+        'browser.startup.page': 0,
+        'browser.tabs.loadOnNewTab': -1,
+        'update.interval': 1000 * 60 * 60 * 24,  # 1 day in milliseconds
+        'update.severity': 2,
+        'update.showSlidingNotification': False,
+        'update_notifications.enabled': False,
+    }
 
     @classmethod
     def parse_override_ini(cls) -> bytes:
@@ -22,6 +142,7 @@ class FirefoxConfig:
         # -override https://wiki.mozilla.org/Firefox/CommandLineOptions#-override_/path/to/override.ini
         # overview of defaults https://github.com/microsoft/playwright/issues/18094
         # source file https://searchfox.org/mozilla-central/source/build/application.ini.in
+        # overview explanation https://virtuallyjason.blogspot.com/2013/10/cutomizing-mozilla-firefox-for.html
         if len(cls.override_ini) == 0:
             return bytes()
         with io.BytesIO() as buffer:
@@ -69,6 +190,9 @@ class FirefoxConfig:
     ) -> bytes:
         # documentation
         # libpref https://firefox-source-docs.mozilla.org/modules/libpref/index.html
+        # User.js file https://kb.mozillazine.org/User.js_file
+        # Locking proferences https://kb.mozillazine.org/Locking_preferences
+        # about:config entries https://kb.mozillazine.org/About:config_entries
         if len(cls.user_js) == 0:
             return bytes()
         string_limit_1MiB = 1024 * 1024
